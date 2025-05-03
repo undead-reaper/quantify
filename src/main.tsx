98b688b8b736e2197ea22396e5834b44ffc448f6
Dharam Soni <dharamsoni1010@gmail.com>
2025-05-04 00:23:27 +0530

    Initial app commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import App from "@/App.tsx";
+import { ThemeProvider } from "@/components/ThemeProvider.tsx";
+import "@/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
